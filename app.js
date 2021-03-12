82a82de7620daf818b3ff220c54b6bdd9a2cb903
Alexander Kramer <akramer@enrise.com>
2021-03-12 14:46:47 +0100

    add favicon

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const favicon = require('serve-favicon')
 const model = require(__dirname + '/model.js')
 const Recepe = model.getRecepeModel()
 
 const app = express();
 const port = 3000;
+
+app.use(favicon(__dirname + '/public/favicon.ico'))
 
 app.listen(port, () => {
   console.log('Express server running on port ' + port)
